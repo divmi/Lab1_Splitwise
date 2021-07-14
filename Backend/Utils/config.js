52e1057e9dcd36dc52b6243378ab98dd6c524203
Divya Mittal <73660339+divmi@users.noreply.github.com>
2021-07-14 12:51:43 -0700

    Update config.js
    Removed access key

--- Backend/Utils/config.js
+++ Backend/Utils/config.js
@@ -2,8 +2,8 @@ const config = {
   secret: "splitwise_Secret_String",
   mongoDB:
     "mongodb+srv://divmi:splitwise@cluster0.wkrel.mongodb.net/SplitwiseDB?retryWrites=true&w=majority",
-  AWSAccessKeyId: "AKIASA26GJPIL5JADT7I",
-  AWSSecretKey: "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",
+  AWSAccessKeyId: "",
+  AWSSecretKey: "",
   AWS_BUCKET_NAME: "splitwiseimage"
 };
 
